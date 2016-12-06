9e03122ebca62a1f035e698618344ff17718ff13
Jonathan Pastor <jonathancmoa@gmail.com>
2016-12-06 10:48:40 -0600

    Update synchronize_hosts_file_master.sh

--- scripts/synchronize_hosts_file_master.sh
+++ scripts/synchronize_hosts_file_master.sh
@@ -8,10 +8,10 @@ WAIT_TIME="5"
 
 while true; do
     LOCAL_HOSTS_FILE_MD5=$(md5sum /etc/hosts | awk '{print $1}')
-    NFS_HOSTS_FILE_MD5=$(md5sum /mnt/nfs/var/nfs/hosts | awk '{print $1}')
+    NFS_HOSTS_FILE_MD5=$(md5sum /var/nfs/hosts | awk '{print $1}')
     if [ "$LOCAL_HOSTS_FILE_MD5" != "$NFS_HOSTS_FILE_MD5" ]; then
         echo "Local /etc/hosts has changed, I will start the synchronization"
-        cp /etc/hosts /mnt/nfs/var/nfs/hosts
+        cp /etc/hosts /var/nfs/hosts
     fi
     echo "Waiting $WAIT_TIME seconds"
     sleep $WAIT_TIME
